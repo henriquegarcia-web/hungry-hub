9779db88a165faf6ea87999592d1acc3b60d2b86
Henrique Garcia <henriquegarcia.dev@gmail.com>
2023-10-07 07:03:50 -0300

    Composição do formulário de suporte

--- client/src/pages/Admin/Views/Support/index.tsx
+++ client/src/pages/Admin/Views/Support/index.tsx
@@ -1,7 +1,245 @@
 import * as S from './styles'
+import { BiSupport } from 'react-icons/bi'
+import { IoChevronDownOutline } from 'react-icons/io5'
+
+import { Button, Dropdown, Form, Input, message, theme } from 'antd'
+
+import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Controller, useForm } from 'react-hook-form'
+
+// import type { MenuProps } from 'antd'
+import { useMemo } from 'react'
+import { useAdminAuth } from '@/contexts/AdminAuthContext'
+
+interface MenuItemWithLabel {
+  label: string
+  key: string
+  disabled?: boolean
+}
+
+const defaultSupportReasons: MenuItemWithLabel[] = [
+  {
+    label: 'Dúvidas Gerais',
+    key: 'support_reason_01'
+  },
+  {
+    label: 'Problemas Técnicos',
+    key: 'support_reason_02'
+  },
+  {
+    label: 'Solicitação de Ajuda com Design',
+    key: 'support_reason_03'
+    // danger: true
+  },
+  {
+    label: 'Perguntas sobre Funcionalidades',
+    key: 'support_reason_04'
+  },
+  {
+    label: 'Ajuda com Gerenciamento de Conteúdo',
+    key: 'support_reason_05'
+  }
+]
+
+interface ISupportSubmitForm {
+  supportReason: any
+  supportSubject: string
+  supportDescription: string
+  supportContactMethod: any
+}
+
+const supportSubmitSchema = Yup.object().shape({
+  supportReason: Yup.mixed().required(),
+  supportSubject: Yup.string().required(),
+  supportDescription: Yup.string().required(),
+  supportContactMethod: Yup.mixed().required()
+})
 
 const Support = () => {
-  return <S.Support>Support</S.Support>
+  const { token } = theme.useToken()
+
+  const { isAdminPremium } = useAdminAuth()
+
+  const { control, handleSubmit, reset, formState } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(supportSubmitSchema)
+  })
+
+  const { isValid } = formState
+
+  const handleSupportSubmit = async (data: ISupportSubmitForm) => {
+    // const deleteAccountResponse = await handleSupportSubmitAccount(data.adminPassword)
+    // if (deleteAccountResponse) {
+    //   reset()
+    // }
+  }
+
+  const premiumSupportReasons: MenuItemWithLabel[] = useMemo(() => {
+    const premiumSupport: MenuItemWithLabel[] = [
+      {
+        label: !isAdminPremium
+          ? 'Integrações Avançadas [Premium]'
+          : 'Integrações Avançadas',
+        key: 'support_reason_06',
+        disabled: !isAdminPremium
+      },
+      {
+        label: !isAdminPremium
+          ? 'Treinamento Avançado [Premium]'
+          : 'Treinamento Avançado',
+        disabled: !isAdminPremium,
+        key: 'support_reason_07'
+      },
+      {
+        label: !isAdminPremium
+          ? 'Consultoria em Marketing [Premium]'
+          : 'Consultoria em Marketing',
+        key: 'support_reason_08',
+        disabled: !isAdminPremium
+      }
+    ]
+
+    const mergedReasons = [...defaultSupportReasons, ...premiumSupport]
+
+    return mergedReasons
+  }, [isAdminPremium])
+
+  const contactMethods: MenuItemWithLabel[] = [
+    {
+      label: 'WhatsApp',
+      key: 'support_contact_01'
+    },
+    {
+      label: 'E-mail',
+      key: 'support_contact_02'
+    }
+  ]
+
+  // const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   message.info('Click on left button.')
+  //   console.log('click left button', e)
+  // }
+
+  const getReasonLabel = (key: string): string | null => {
+    const item = premiumSupportReasons.find((item) => item.key === key)
+    return item ? item.label : null
+  }
+
+  const getContactLabel = (key: string): string | null => {
+    const item = contactMethods.find((item) => item.key === key)
+    return item ? item.label : null
+  }
+
+  return (
+    <S.Support>
+      <S.SupportWrapper>
+        <S.SupportForm style={{ border: `1px solid ${token.colorBorder}` }}>
+          <S.SupportFormHeader
+            style={{
+              backgroundColor: token.colorBgContainer,
+              borderBottom: `1px solid ${token.colorBorder}`
+            }}
+          >
+            <S.SupportFormHeaderIcon style={{ color: token.colorTextHeading }}>
+              <BiSupport />
+            </S.SupportFormHeaderIcon>
+            <S.SupportFormHeaderLabel style={{ color: token.colorTextHeading }}>
+              Suporte
+            </S.SupportFormHeaderLabel>
+          </S.SupportFormHeader>
+          <S.SupportFormContent
+            layout="vertical"
+            onFinish={handleSubmit(handleSupportSubmit)}
+          >
+            <Form.Item label="Motivo do suporte">
+              <Controller
+                name="supportReason"
+                control={control}
+                render={({ field }) => (
+                  <Dropdown.Button
+                    menu={{
+                      items: premiumSupportReasons,
+                      onClick: (e) => field.onChange(e.key),
+                      style: { width: '100%' }
+                    }}
+                    icon={
+                      <IoChevronDownOutline
+                        style={{ fontSize: 16, marginBottom: '-4px' }}
+                      />
+                    }
+                    trigger={['click']}
+                    // onClick={handleButtonClick}
+                  >
+                    {field.value
+                      ? getReasonLabel(field.value.toString())
+                      : 'Selecione um motivo'}
+                  </Dropdown.Button>
+                )}
+              />
+            </Form.Item>
+            <Form.Item label="Assunto">
+              <Controller
+                name="supportSubject"
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    placeholder="Digite o assunto principal da solicitação"
+                  />
+                )}
+              />
+            </Form.Item>
+            <Form.Item label="Detalhes">
+              <Controller
+                name="supportDescription"
+                control={control}
+                render={({ field }) => (
+                  <Input.TextArea
+                    {...field}
+                    placeholder="Digite os detalhes da solicitação"
+                    rows={6}
+                    style={{ resize: 'none' }}
+                  />
+                )}
+              />
+            </Form.Item>
+            <Form.Item label="Preferência de contato">
+              <Controller
+                name="supportContactMethod"
+                control={control}
+                render={({ field }) => (
+                  <Dropdown.Button
+                    menu={{
+                      items: contactMethods,
+                      onClick: (e) => field.onChange(e.key),
+                      style: { width: '100%' }
+                    }}
+                    icon={
+                      <IoChevronDownOutline
+                        style={{ fontSize: 16, marginBottom: '-4px' }}
+                      />
+                    }
+                    trigger={['click']}
+                    // onClick={handleButtonClick}
+                  >
+                    {field.value
+                      ? getContactLabel(field.value.toString())
+                      : 'Selecione uma forma de contato'}
+                  </Dropdown.Button>
+                )}
+              />
+            </Form.Item>
+            <S.FormFooter>
+              <Button type="primary" htmlType="submit" disabled={!isValid}>
+                Enviar
+              </Button>
+            </S.FormFooter>
+          </S.SupportFormContent>
+        </S.SupportForm>
+      </S.SupportWrapper>
+    </S.Support>
+  )
 }
 
 export default Support
--- client/src/pages/Admin/Views/Support/styles.ts
+++ client/src/pages/Admin/Views/Support/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
-import { View, adminViewInfosWrapper } from '@/utils/styles/globals'
+import { View, adminViewSupportWrapper } from '@/utils/styles/globals'
+import { Form } from 'antd'
 
 export const Support = styled(View)`
   display: flex;
@@ -12,7 +13,7 @@ export const SupportWrapper = styled.div`
   flex-direction: column;
   row-gap: 20px;
   width: 100%;
-  max-width: ${adminViewInfosWrapper};
+  max-width: ${adminViewSupportWrapper};
   height: fit-content;
 
   /* border: 1px solid red; */
@@ -21,3 +22,80 @@ export const SupportWrapper = styled.div`
 // export const CompanyInfos = styled.div`
 //   display: flex;
 // `
+
+// ========================================== SUPPORT CONTAINER
+
+export const SupportForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: fit-content;
+  overflow: hidden;
+
+  border-radius: 10px;
+`
+
+export const SupportFormHeader = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 8px;
+  padding: 15px;
+`
+
+export const SupportFormHeaderIcon = styled.div`
+  display: flex;
+
+  svg {
+    font-size: 18px;
+  }
+`
+
+export const SupportFormHeaderLabel = styled.h2`
+  display: flex;
+  flex: 1;
+
+  font-size: 17px;
+  line-height: 17px;
+  font-weight: 500;
+`
+
+export const SupportFormHeaderIncomplete = styled.h2`
+  display: flex;
+`
+
+export const SupportFormContent = styled(Form)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+  padding: 15px;
+
+  .ant-btn-compact-first-item {
+    flex: 1 !important;
+  }
+
+  .ant-form-item {
+    margin-bottom: 0px;
+  }
+`
+
+export const SupportFormLoading = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(2px);
+`
+
+export const FormFooter = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  margin-top: 10px;
+`
